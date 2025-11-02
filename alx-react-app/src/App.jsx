af08eb2b8c88bf92722876ec101041686da788c8
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-02 12:33:00 +0300

    task 2 edites

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-import UserProfile from './components/UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      {/* ...other components... */}
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
