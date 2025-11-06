0c4c25e863c2112e41a198f41d4d458d2e4d23b2
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-06 21:58:41 +0300

    task 3 done

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ fontFamily: 'Arial, sans-serif' }}>
+        <Navbar />
+        <div style={{ marginTop: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
