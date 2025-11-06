9d37f058784378584edd31dd23e143c560c8de35
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-06 22:27:13 +0300

    task 2 done

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -4,11 +4,14 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Simple Counter</h2>
       <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)} style={{ margin: '0 10px' }}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
     </div>
   );
 }
