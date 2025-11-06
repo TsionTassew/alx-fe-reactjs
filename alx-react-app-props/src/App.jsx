5f7f89a0cb02dee04df3236f517d68b742ef8c6d
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-06 19:53:37 +0300

    task 2 done agin

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,6 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from "./UserContext";
+import ProfilePage from "./ProfilePage";
+// or "UserProfile" if that is what your assignment expects!
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
