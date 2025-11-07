957944bc11fc322168b0d2d9b3eaab83360a9e90
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-07 19:32:29 +0300

    task 1 done

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,16 @@
-// src/App.jsx
 import React from "react";
 import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
-import WelcomeMessage from "./components/WelcomeMessage"; 
 
 function App() {
   return (
     <div>
       <h1>User Profile Card</h1>
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
+      <WelcomeMessage />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
       />
     </div>
   );
