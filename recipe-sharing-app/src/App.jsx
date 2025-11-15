24c676498a52e51aab132217bbea13b583e60eff
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-15 20:32:02 +0300

    task 1 done

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,43 @@
-import { Link } from 'react-router-dom';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import { useRecipeStore } from './recipeStore';
-
+import RecipeDetails from './components/RecipeDetails'; // make sure you have this component
 
 function App() {
-const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
 
-return (
-<div>
-<h1>Recipe Sharing App</h1>
-<AddRecipeForm />
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path="/"
+            element={
+              <div>
+                <AddRecipeForm />
+                {recipes.map((recipe) => (
+                  <div key={recipe.id}>
+                    <h3>{recipe.title}</h3>
+                    <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+                  </div>
+                ))}
+              </div>
+            }
+          />
 
-
-{recipes.map((recipe) => (
-<div key={recipe.id}>
-<h3>{recipe.title}</h3>
-<Link to={`/recipe/${recipe.id}`}>View Details</Link>
-</div>
-))}
-</div>
-);
+          {/* Recipe Details Page */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-
 
 export default App;
