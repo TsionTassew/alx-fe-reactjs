a5d39feffde029fd7f164b1c17c3e9e434a16fd3
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-16 20:21:40 +0300

    task 2 done

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // src/components/RecipeList.jsx
-import { useRecipeStore } from '../recipeStore';
+import { Link } from "react-router-dom";
+import { useRecipeStore } from "../recipeStore";
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
@@ -10,6 +11,9 @@ const RecipeList = () => {
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+
+          {/* Link to recipe details page */}
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
