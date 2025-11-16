e5457fccd03e713d9cd385094045845a958786e5
Tsion Tassew <tsiontassew91@gmail.com>
2025-11-16 20:14:27 +0300

    task 2 done

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,21 +1,19 @@
+// src/components/RecipeList.jsx
 import { useRecipeStore } from '../recipeStore';
 
-
 const RecipeList = () => {
-const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
-
-return (
-<div>
-{recipes.map((recipe) => (
-<div key={recipe.id}>
-<h3>{recipe.title}</h3>
-<p>{recipe.description}</p>
-</div>
-))}
-</div>
-);
+  return (
+    <div>
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
-
 
 export default RecipeList;
