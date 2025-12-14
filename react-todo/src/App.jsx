85da824e0b62783a87b1263b794da24e36493c18
Tsion Tassew <tsiontassew91@gmail.com>
2025-12-14 10:17:27 +0300

    task 3 done

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,4 +1,8 @@
 
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+import userEvent from "@testing-library/user-event";
+
 import TodoList from "./components/TodoList";
 
 function App() {
